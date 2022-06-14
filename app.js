88573499f340cc02ba8279c38b0c58c1b85b529c
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-06-14 14:01:45 -0700

    Challenge 7: Compose page.

--- app.js
+++ app.js
@@ -32,7 +32,9 @@ app.get("/contact", function(req, res) {
   res.render("contact", {contactContent: contactContent});
 });
 
-
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
 
 // Initialize server.
 
